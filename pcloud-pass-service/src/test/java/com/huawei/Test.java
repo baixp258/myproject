2d0682a32331fbd54951b6e24b7760d7dc5b8d83
baixp258 <13630876201@139.com>
2021-12-09 13:47:21 +0800

    service添加测试Test测试类

--- pcloud-pass-service/src/test/java/com/huawei/Test.java
+++ pcloud-pass-service/src/test/java/com/huawei/Test.java
@@ -4,6 +4,7 @@ public class Test {
         public static void main(String args[]) {
             //1.测试类
              System.out.println("第一次提交");
-
+            //2.第二次提交
+             System.out.println("第二次提交");
         }
 }
